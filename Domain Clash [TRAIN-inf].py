5db506800deec6bba1fbff43c992031c2c9fb049
Masergun <126848167+Masergun@users.noreply.github.com>
2024-03-01 07:27:41 -0800

    Update Domain Clash [TRAIN-inf].py
    Оптимизировал

--- Domain Clash [TRAIN-inf].py
+++ Domain Clash [TRAIN-inf].py
@@ -14,9 +14,13 @@ GREEN = (0, 255, 0)
 BLACK=(0, 0, 0)
 
 WASD_mass = ["w", "a", "s", "d"]
-WASD_mass = [random.choice(WASD_mass) for _ in range(100000)]
-
+WASD_nado=[]
+for _ in range(10):
+    WASD_nado.append(random.choice(WASD_mass))
+print(WASD_nado)
 SCORE=0
+
+a=0
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Domain Clash")
@@ -41,25 +45,28 @@ while running:
         elif event.type == pygame.KEYDOWN:
             end_time = time.time()
             total_time = end_time - start_time
-            if event.unicode == WASD_mass[current_index]:
+            if len(WASD_nado) < 21:
+                WASD_nado.append(random.choice(WASD_mass))
+            if len(WASD_nado)> 11:
+                WASD_nado.pop(0)
+                current_index = current_index - 1
+
+            if event.unicode == WASD_nado[current_index]:
+                if a == 0:
+                    SCREEN_WIDTH -= 100
+                    a=1
                 print("Correct key pressed:", event.unicode)
                 current_index += 1  # Переходим к следующему индексу
-                SCREEN_WIDTH=SCREEN_WIDTH-100
                 SCORE=SCORE+1
 
-                if current_index == len(WASD_mass):
-                    print("All keys pressed correctly! Total time:", total_time)
-                    print("Всё верно! Затраченое время:", total_time)
-                    running = False
             else:
                 print("Wrong key pressed:", event.unicode)
                 print("Score:", SCORE)
                 print("Попробуй ещё раз! Затраченое время:", total_time)
                 running = False
-
     screen.fill(WHITE)
 
-    for i, key in enumerate(WASD_mass):
+    for i, key in enumerate(WASD_nado):
         text_color = GREEN if i < current_index else RED  # Зеленый для правильно нажатых, красный для остальных
         text_surface = font.render(key, True, text_color)
         text_rect = text_surface.get_rect(center=(SCREEN_WIDTH // 2 + i * 50, SCREEN_HEIGHT // 2))
